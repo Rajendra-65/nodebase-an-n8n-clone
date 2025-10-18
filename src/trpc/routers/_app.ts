24d801a38e9c5935000a1c382e5fa7cea50ba3e4
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-10-18 16:38:29 +0530

    Added the background job inngest

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,22 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
+import { inngest } from '@/inngest/client';
 // for creating the APIroutes..
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ctx}) => {
+  getUsers: protectedProcedure.query(async ({ctx}) => {
+      await inngest.send({
+        name : "test/hello.world",
+        data : {
+          email : "rajendra@gmail.com"
+        }
+      })
       return prisma.user.findMany({
         where : {
           id : ctx.auth.user.id
         }
       })
-    }),
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
